95c2920d1768bdc179155c72ba880d37f9133883
DinoBambinio <117571267+DinoBambinio@users.noreply.github.com>
2022-12-04 12:21:01 +0300

    Update Mobile.java

--- src/Mobile.java
+++ src/Mobile.java
@@ -2,9 +2,9 @@ public class Mobile {
     public static void main(String args[]) {
         int mani = 100;
         int replenishment = 1100;
-        if (replenishment >= 1000) {
+        if (replenishment = 1000) {
             System.out.println("Ваш баланс " + ((replenishment + mani) + (replenishment / 100)) + " рублей");
-        } else if (replenishment < 1000) {
+        } else if (replenishment <= 1000) {
             System.out.println("Ваш баланс " + (replenishment + mani) + " рублей");
         }
 
